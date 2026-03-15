2b7e87176cc8b6c29314bb68076434c7e76664d1
Wen Ting Chua <wentingchua@gmail.com>
2026-03-16 00:26:27 +0800

    Include WhiteboardModule in apps.module

--- apps/collaboration-service/src/app.module.ts
+++ apps/collaboration-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SessionsModule } from './sessions/sessions.module';
+import { WhiteboardModule } from './whiteboard/whiteboard.module';
 
 @Module({
-  imports: [SessionsModule],
+  imports: [SessionsModule, WhiteboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
