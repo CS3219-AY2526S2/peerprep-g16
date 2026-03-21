78ca7c824bc4a4fc4ff3d83845e100ea0fe8ab7b
wenting <wentingchua@gmail.com>
2026-03-21 22:25:28 +0800

    Update settings

--- apps/collaboration-service/src/app.module.ts
+++ apps/collaboration-service/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SessionsModule } from './sessions/sessions.module';
 import { WhiteboardModule } from './whiteboard/whiteboard.module';
 
 @Module({
-  imports: [SessionsModule, WhiteboardModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SessionsModule,
+    WhiteboardModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
