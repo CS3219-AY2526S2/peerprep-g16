a1d7fa1c92bf3627a98cd61e5eb0525c7b3bd056
wenting <wentingchua@gmail.com>
2026-04-16 01:21:49 +0800

    Update secrets.tf

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -23,6 +23,7 @@ resource "aws_secretsmanager_secret_version" "peerprep_secrets_version" {
     ENCRYPTION_KEY        = "placeholder"
     ENCRYPTION_IV         = "placeholder"
     QUESTION_SERVICE_MONGODB_URI = "placeholder"
+    COLLAB_MONGODB_URI = "placeholder"
     JUDGE0_POSTGRES_DB    = "placeholder"
     JUDGE0_POSTGRES_USER  = "placeholder"
     JUDGE0_POSTGRES_PASSWORD = "placeholder"
